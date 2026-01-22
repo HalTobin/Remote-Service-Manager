f9ba02807bc5545efa2bf422ffdad50c40700423
Alexis Aneas <alexis.aneas@outlook.fr>
2026-01-22 19:02:27 +0100

    starting to work on the ui to manage ssh keys

---
+++ feature/auth/lib/auth/feature/my_ssh_keys/presentation/component/delete_button.dart
@@ -0,0 +1,33 @@
+import 'package:lucide_icons_flutter/lucide_icons.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class DeleteButton extends StatelessWidget {
+  final bool enable;
+  final Function() onClick;
+
+  const DeleteButton({
+    super.key,
+    required this.enable,
+    required this.onClick
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ConstrainedBox(
+      constraints: const BoxConstraints(maxWidth: 384),
+      child: SizedBox(
+        height: 40,
+        child: OutlinedButton.icon(
+          onPressed: () => {
+            if (enable) {
+              onClick()
+            }
+          },
+          icon: const Icon(LucideIcons.trash),
+          label: Text("Delete")
+        )
+      )
+    );
+  }
+}
--- feature/auth/lib/auth/feature/my_ssh_keys/presentation/my_ssh_keys_view.dart
+++ feature/auth/lib/auth/feature/my_ssh_keys/presentation/my_ssh_keys_view.dart
@@ -1,5 +1,35 @@
+import 'package:lucide_icons_flutter/lucide_icons.dart';
+import 'package:feature_auth/auth/feature/my_ssh_keys/presentation/my_ssh_keys_event.dart';
+import 'package:feature_auth/auth/feature/my_ssh_keys/presentation/my_ssh_keys_state.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:ui/component/title_header.dart';
 
 class MySshKeysView extends StatelessWidget {
+  final MySshKeysState state;
+  final Function(MySshKeysEvent) onEvent;
+  final Function(String) onSelect;
 
+  const MySshKeysView({
+    super.key,
+    required this.state,
+    required this.onEvent,
+    required this.onSelect
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        TitleHeader(
+          icon: LucideIcons.folderKey,
+          title: "My SSH Keys"
+        ),
+        Row(
+          children: [
+
+          ],
+        )
+      ],
+    );
+  }
 }
